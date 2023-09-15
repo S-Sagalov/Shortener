336149173081919fe827ee8dda7a517db5949c33
S-Sagalov <s.sagalov@mail.ru>
2023-09-15 21:47:36 +0500

    first iteration

--- settings.py
+++ settings.py
@@ -11,5 +11,5 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI',
                                         default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
 
