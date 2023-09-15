8941b0b38b749956e62caba9235fbe0bbe494d09
S-Sagalov <s.sagalov@mail.ru>
2023-09-15 21:47:25 +0500

    first iteration

--- settings.py
+++ settings.py
@@ -1,7 +1,6 @@
 import os
 from string import ascii_lowercase, ascii_uppercase
 
-
 LINK_LENGTH = 6
 POSSIBLE_SYMBOLS = ascii_lowercase + ascii_uppercase + '0123456789'
 
@@ -9,6 +8,8 @@ POSSIBLE_SYMBOLS = ascii_lowercase + ascii_uppercase + '0123456789'
 # было бы логичнее использовать set, но не пускают автотесты
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI',
+                                        default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
+
